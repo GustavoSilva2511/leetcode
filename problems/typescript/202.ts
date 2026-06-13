1134bccd53c1eb0d480ee81618d1a233651961d7
GustavoSilva2511 <gustavooficial2511@gmail.com>
2026-06-13 11:37:51 -0300

    add: typescript 202 solution incomplete

---
+++ problems/typescript/202.ts
@@ -0,0 +1,21 @@
+function isHappy(n: number): boolean {
+    let currNumber: string = n.toString();
+    const ocurrences: string[] = [];
+
+    while (currNumber != "1") {
+        if (ocurrences.includes(currNumber)) {
+            return false;
+        }
+        ocurrences.push(currNumber);
+
+        const newNumberList = currNumber.split("").map(char => Math.pow(parseInt(char), 2));
+
+        currNumber = newNumberList.reduce((acumulator, currentValue) => {
+            return acumulator + currentValue;
+        }, 0);
+    }
+
+    return true;
+};
+
+console.log(isHappy(2));
